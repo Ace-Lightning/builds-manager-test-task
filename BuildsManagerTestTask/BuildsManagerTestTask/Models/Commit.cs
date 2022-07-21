ef2a29390311c08614d98088ee3c33b80df3cc1f
Sergey Koryshev <sergey.koryshev@outlook.com>
2022-07-21 07:44:19 +0200

    Corrected models

--- BuildsManagerTestTask/BuildsManagerTestTask/Models/Commit.cs
+++ BuildsManagerTestTask/BuildsManagerTestTask/Models/Commit.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BuildsManagerTestTask.Models;
+﻿namespace BuildsManagerTestTask.Models;
 
 internal class Commit
 {
